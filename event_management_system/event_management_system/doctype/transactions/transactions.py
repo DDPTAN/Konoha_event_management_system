7f9462fcf5abe2cee967da7fa55d7c3f5ebe5a25
aswar-tan <aswar@tandigital.id>
2024-06-21 13:39:08 +0800

    chore(doctype): create doctype events and transactions

---
+++ event_management_system/event_management_system/doctype/transactions/transactions.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, team konoha and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Transactions(Document):
+	pass
